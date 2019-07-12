f93da1ed0950748d4107ca32b1d7f32e01dd553e
Wesley Jones <monsterzero7@gmail.com>
2019-07-12 14:12:49 -0400

    IP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './products/product/product.component';
 import { FavoriteComponent } from './products/favorite/favorite.component';
+import { UserService } from './services/user.service';
+import { ProductService } from './services/product.service';
+import { PurchaseService } from './services/purchase.service';
+import { TaxonomyService } from './services/taxonomy.service';
+import { InterestService } from './services/interest.service';
+import { FavoriteService } from './services/favorite.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +24,18 @@ import { FavoriteComponent } from './products/favorite/favorite.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+    ProductService,
+    PurchaseService,
+    TaxonomyService,
+    InterestService,
+    FavoriteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email: String, password:String): Observable<User> {
+
+  }
+
+
+
 }
