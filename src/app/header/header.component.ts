58da8b0b35b53cc81a3beaf58f8aa68fc85cc552
Wesley Jones <monsterzero7@gmail.com>
2019-07-15 21:14:37 -0400

    *ngIf used in navbar html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   menuToggle = false;
-  
-  constructor() { }
 
+  constructor(private userService: UserService) { }
+  logout(){
+    this.userService.logout();
+  }
   ngOnInit() {
   }
 
