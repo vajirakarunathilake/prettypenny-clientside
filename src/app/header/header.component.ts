d4249de2dbb7bd3302ae40ecfb5f744860d10d48
Wesley Jones <monsterzero7@gmail.com>
2019-07-15 21:45:16 -0400

    created localStorage helper for global local storage, this will help with our if statements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { Helpers } from '../helpers';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +10,15 @@ import { UserService } from '../services/user.service';
 export class HeaderComponent implements OnInit {
   menuToggle = false;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, public helper: Helpers) { }
   logout(){
     this.userService.logout();
   }
+  readLocalStorageValue(key) {
+    return localStorage.getItem(key);
+}
   ngOnInit() {
+    localStorage.setItem('check', '0');
   }
 
 }
