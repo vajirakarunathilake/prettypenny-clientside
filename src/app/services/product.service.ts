01603c414eb2204b6e4e7f96650111f8c94167f4
Vajira Hapu Arachchige <51335173+vajirakarunathilake@users.noreply.github.com>
2019-07-16 12:59:34 -0400

    insert Product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -42,6 +42,12 @@ export class ProductService {
       { name: productName, price: productPrice, userId: id, interestThreshold: interestThreshold, imageUrl: image })
       .pipe(map((response: any) => response));
   }
+  insertt(product: Product): Observable<any> {
+    console.log(product);
+    
+    return this.http.post(`${environment.apiBase}/product`, product)
+      .pipe(map((response: any) => response));
+  }
 
   update(productName: string, productPrice: number, salePrice: number, prodStatus: string, id: number, interestThreshold: number, image: string): Observable<any> {
     return this.http.put(`${environment.apiBase}/product`,
