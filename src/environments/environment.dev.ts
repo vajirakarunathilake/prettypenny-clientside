aead17947751a376aa527b1209e0272709bc84d4
mjrerle <mjrerle@gmail.com>
2019-07-20 12:26:58 -0400

    fix homepage problems

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  mockApiBase: 'https://ngshop-27da4.firebaseio.com',
   apiBase: 'http://localhost:8080/project2-backend/api',
+  homeURI: 'http://localhost:4200/',
   brands: ['Apple', 'Samsung', 'Sony', 'Dell'],
   categories: ['Electronics', 'Cloths', 'Watches', 'Furnitures'],
   subCategories: ['TV', 'Phone', 'Shirts', 'Chairs']
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@
 export const environment = {
   production: false,
   apiBase: 'http://localhost:8080/project2-backend/api',
+  homeURI: 'http://localhost:4200/',
   brands: ['Apple', 'Samsung', 'Sony', 'Dell'],
   categories: ['Electronics', 'Cloths', 'Watches', 'Furnitures'],
   subCategories: ['TV', 'Phone', 'Shirts', 'Chairs']
