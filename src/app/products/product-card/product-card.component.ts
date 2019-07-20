241a0c70e2691ac50552e146a0872615825f5a2f
mjrerle <mjrerle@gmail.com>
2019-07-20 13:49:58 -0400

    grammar and consistency

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -17,7 +17,7 @@ export class ProductCardComponent implements OnInit {
   quantity = 1;
   loggedIn = false;
   pretty = false;
-  precentage: number;
+  percentage: number;
   needthresholdtext: string;
   over: number;
   need: number;
@@ -32,8 +32,8 @@ export class ProductCardComponent implements OnInit {
   ngOnInit() {
     this.loggedIn = this.helper.localStorageItem('email') != null;
     this.pretty = this.product.status === 'Pretty' ? true : false;
-    this.precentage = (this.product.generatedInterest / this.product.interestThreshold) * 100;
-    if (this.precentage > 100) {
+    this.percentage = (this.product.generatedInterest / this.product.interestThreshold) * 100;
+    if (this.percentage > 100) {
       this.needClass = 'text-primary';
       this.over = this.product.generatedInterest - this.product.interestThreshold;
       this.needthresholdtext = 'Over ' + this.over + ' Sold';
